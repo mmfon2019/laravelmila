7d598771edd88b023e0cb5e1d6a8f802460e671b
milicaa1405 <mm20190327@student.fon.bg.ac.rs>
2023-02-12 15:19:46 +0100

    Kreirani modeli User, Series, Genre i Starring međusobno povezani

---
+++ store/app/Models/Series.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Series extends Model
+{
+    use HasFactory;
+
+    protected $guarded=['id'];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function starring()
+    {
+        return $this->belongsTo(Starring::class);
+    }
+}
