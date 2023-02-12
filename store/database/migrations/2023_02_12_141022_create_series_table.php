c66fa45a66f96aba8481869adc72452d99f1f223
milicaa1405 <mm20190327@student.fon.bg.ac.rs>
2023-02-12 15:13:30 +0100

    Kreirane migracije za kreiranje tabela i dodavanje kolone u tabeli series

---
+++ store/database/migrations/2023_02_12_141022_create_series_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+            $table->foreignId('genre_id');
+            $table->foreignId('starring_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
