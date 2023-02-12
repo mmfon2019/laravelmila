36ec7999ea03c30c3c5c3fb345263762c818236e
milicaa1405 <mm20190327@student.fon.bg.ac.rs>
2023-02-12 15:36:42 +0100

    Kreirane api rute u fajlu api.php

--- store/routes/api.php
+++ store/routes/api.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\UserSeriesController;
+use App\Http\Controllers\GenreSeriesController;
+use App\Http\Controllers\StarringController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\StarringSeriesController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +25,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('users.series',UserSeriesController::class)->only(['index']);
+Route::resource('genres.series', GenreSeriesController::class)->only(['index']);
+Route::resource('starrings.series', StarringSeriesController::class)->only(['index']);
+
+Route::post('/register',[AuthController::class,'register']);
+
+Route::post('/login',[AuthController::class,'login']);
+
+Route::group(['middleware'=>['auth:sanctum']], function() {
+    Route::get('/profiles',function(Request $request) {
+        return auth()->user();
+    });
+    Route::resource('series', SeriesController::class)->only(['update','store','destroy']);
+    Route::resource('genres', GenreController::class)->only(['update','store','destroy']);
+    Route::resource('starrings', StarringController::class)->only(['update','store','destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
+});
+
+Route::resource('series', SeriesController::class)->only(['index','show']);
+Route::resource('genres', GenreController::class)->only(['index','show']);
+Route::resource('starrings', StarringController::class)->only(['index','show']);
